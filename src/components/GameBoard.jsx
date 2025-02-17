0a3b786564644f92a3a18410b23b0e9ba98c28ce
Anish Shukla <shuklaanish040@gmail.com>
2025-02-17 19:53:23 +0530

    xo

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -11,18 +11,23 @@ export default function GameBoard() {
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameBoard((prevGameBoard) => {
-      prevGameBoard[rowIndex][colIndex] = "X";
-      return prevGameBoard;
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
     });
   }
   return (
     <ol id="game-board">
-      {intialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>

60a187a4206b3b6ec9ce2b1fe436d0a829134cd4
Anish Shukla <shuklaanish040@gmail.com>
2025-02-17 19:29:08 +0530

    xo

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const intialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,6 +7,14 @@ const intialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(intialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      prevGameBoard[rowIndex][colIndex] = "X";
+      return prevGameBoard;
+    });
+  }
   return (
     <ol id="game-board">
       {intialGameBoard.map((row, rowIndex) => (
